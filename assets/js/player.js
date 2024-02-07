67648588f3153e9e9c5ccbfe3fa258334d971022
nooptek <nooptek@proton.me>
2024-02-07 23:38:54 +0100

    Add more play speeds

--- assets/js/player.js
+++ assets/js/player.js
@@ -5,7 +5,7 @@ var video_data = JSON.parse(document.getElementById('video_data').textContent);
 var options = {
     preload: 'auto',
     liveui: true,
-    playbackRates: [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0],
+    playbackRates: [0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 4.0],
     controlBar: {
         children: [
             'playToggle',
